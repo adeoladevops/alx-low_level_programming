60888f84c678d5027a733b7bcb60aa0b6f142ce8
adeoladevops <iloriabdulqoyum@gmail.com>
2023-02-18 13:54:48 -0800

    4thprogram

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-int main() {
-    char message[] = "Programming is like building a multilingual puzzle";
-    puts(message);
-    return (0);
+int main(void) 
+{
+	char message[] = "Programming is like building a multilingual puzzle \n";
+
+	puts(message);
+	return (0);
 }
