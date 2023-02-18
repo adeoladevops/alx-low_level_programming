50280b077d8ca5a77ac4bfdc059d0ceeb0ba6079
adeoladevops <iloriabdulqoyum@gmail.com>
2023-02-18 14:07:02 -0800

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+/** main - print a sentence without using printf
+ *
+ *Return: 0
+ */
 
 int main(void) 
 {
-	char message[] = "Programming is like building a multilingual puzzle \n";
-
+	char message[] ="\"Programming is like building a multilingual puzzle";
 	puts(message);
 	return (0);
 }
