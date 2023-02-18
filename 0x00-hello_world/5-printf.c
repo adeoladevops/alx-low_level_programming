0e10046815921291ac5b04596d0b907c559e09d6
adeoladevops <iloriabdulqoyum@gmail.com>
2023-02-18 14:52:33 -0800

    5 done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - we are using prinft here
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
