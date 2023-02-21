f4b738075882a3c8f4a332f4deaa56e4ab258627
adeoladevops <iloriabdulqoyum@gmail.com>
2023-02-20 17:29:30 -0800

    newt

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,18 +19,22 @@ int main(void)
 	printf("Last digit of %d is %d", n, a);
 	if (a > 5)
 	{
-		printf(" and is greater than 5");
+
+		printf(" and is greater than 5 ");
 
 	}
 	else if (a == 0)
 	{
-		printf(" and is 0");
+
+		printf(" and is 0 ");
 	}
 	else if (a < 6 && a != 0)
 	{
-		printf(" and is less than 6 and not 0");
+
+		printf(" and is less than 6 and not 0 ");
 	}
 	else
+
 		printf("\n");
 
 	return (0);
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,14 +6,15 @@
  * Return: Always 0 (Success)
  */
 
-int main()
+int main(void)
 {
 	char letter = 'a';
-	while (letter <= 'z') 
+	while (letter <= 'z')
+
 	{
-	       	putchar(letter);
+		putchar(letter);
 		letter++;
 	}
 	putchar('\n');
-	return 0;
+	return (0;
 }
