ad752099ad43a7010a843cbad590a08827af9118
adeoladevops <iloriabdulqoyum@gmail.com>
2023-02-27 20:58:25 +0100

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 #endif
