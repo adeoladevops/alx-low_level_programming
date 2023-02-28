e0f06c8f6807fd0324f4e2f312e4ddfe3abec153
adeoladevops <iloriabdulqoyum@gmail.com>
2023-02-28 12:48:30 +0100

    fixed

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *reset_to_98 - using pointer to update the var of n
+ * @n: value of what is stored in the address &n
+ *Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
