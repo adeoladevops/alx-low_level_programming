5543491c8e25684f4113f45d9a5ab3065bf1577b
adeoladevops <iloriabdulqoyum@gmail.com>
2023-02-28 14:36:23 +0100

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  _strlen - return length of characters
+ *  @s: character to check
+ *  Return: 0 always
+ */
+
+int _strlen(char *s)
+{
+	 int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+
+	return (i);
+
+}
